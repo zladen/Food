654b2466301b84f6bc0f1a9601c4c1aece833fcd
banderlog2013 <banderlog2013@yandex.ru>
2022-08-10 23:34:58 +0300

    update_timer

--- js/modules/timer.js
+++ js/modules/timer.js
@@ -1,11 +1,20 @@
 function timer(id, deadline) {
 
     function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
             days = Math.floor( (t/(1000*60*60*24)) ),
             seconds = Math.floor( (t/1000) % 60 ),
             minutes = Math.floor( (t/1000/60) % 60 ),
             hours = Math.floor( (t/(1000*60*60) % 24) );
+        }
 
         return {
             'total': t,
